db9f026fc19e2f1d3d2a73378ae3b636297ddd4c
awinterm13 <andrew.wintermyer@protonmail.com>
2021-07-11 05:09:01 -0500

    made a list of items component. aw

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseList from "./components/ExpenseList";
 
 function App() {
   const expenses = [
@@ -21,33 +21,19 @@ function App() {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
+    {
+      id: "e1",
+      title: "goldfish",
+      amount: 14.69,
+      date: new Date(2020, 7, 12),
+    },
   ];
-
+ 
   return (
     <div>
       <h2>Hello World!</h2>
       <p>This is also a thing on the screen.</p>
-
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <ExpenseList expenses={expenses}/>
     </div>
   );
 }
